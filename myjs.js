cc63443a101b3c925dd48d3f875b4e3d68cd2782
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-06-20 10:27:30 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,8 @@
 alert('Hello, Kota!');
 alert(1+1);
 alert(Math.sqrt(1+1));
+alert(Math.sqrt(2+3));
+alert(Math.sqrt('2'+'3'));
+alert(Math.sqrt(Number('2')+Number('3')));
+alert(Math.sqrt('2'+3));
+alert(Math.sqrt(Number('2')+3));
